0f3fd0d912511da8111caae8b14a91696fa2c3c0
konflic <konflic@mail.ru>
2020-01-12 22:39:40 +0300

    Версия 2.0

---
+++ 4_api_testing/test_api_schema_example.py
@@ -0,0 +1,17 @@
+from jsonschema import validate
+
+def test_api_json_schema(api_client):
+    res = api_client.get(path="/todos/1")
+
+    schema = {
+        "type": "object",
+        "properties": {
+            "id": {"type": "number"},
+            "userId": {"type": "number"},
+            "title": {"type": "string"},
+            "completed": {"type": "boolean"}
+        },
+        "required": ["id", "userId", "title", "completed"]
+    }
+
+    validate(instance=res.json(), schema=schema)
